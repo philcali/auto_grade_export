74bb56b1ef8a02f1aab57f299763235cd2f29817
Philip Cali <philip.cali@gmail.com>
2012-07-18 07:59:31 -0500

    Update resume and build script

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 
-if [ -f out/css/bootstrap.css ]
+if [ -f out/css/bootstrap.css ]; then
   echo Bootstrap exists.
-then
+else
   echo Compiling bootstrap.
   mkdir -p out/css
+  mkdir out/images
   lessc bootstrap/less/bootstrap.less > out/css/bootstrap.css
 fi
 
+echo Moving images
+
+cp -r images/* out/images
 lmxml $1 > out/index.html
